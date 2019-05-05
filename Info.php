fb7591fdd76f07b8687a844eb2b3c2c36e2b6069
phatnguyentien <phatnguyentien@live.com>
2019-05-05 23:44:06 +0700

    Add more features

--- Info.php
+++ Info.php
@@ -1,3 +1,31 @@
-<?php 
-	echo "Đây là website đặt vé máy bay trực tuyến";
-?>
+<html>
+	<head>
+	<style type="text/css">
+	
+	
+	
+	</style>
+	<head>
+	<body>
+	<form>
+		<center>
+		<h2> Website đặt vé máy bay trực tuyến <id="info" type='text'/><h2><br>
+		</center>
+	</form>
+	<form>
+			<br></br>
+			<div class="left" style="float:left;width:45%;margin-right:5%">
+			<img src="3.png">
+			</div>
+			<b> Là công ty du lịch trực tuyến hàng đầu. đáp ứng đa dạng các nhu cầu du lịch trên cùng một nền tảng, giúp khách hàng có được những chuyến du lịch tuyệt vời với người thân yêu.</b>
+			<br></br>
+			<b> Tìm vé máy bay giá rẻ: Chúng tôi giúp bạn đặt vé máy bay mà không phải thanh toán bất kỳ khoản phí trung gian nào. Sau khi tìm được vé máy bay giá rẻ nhất cho chuyến bay đã lựa chọn, bạn sẽ đặt trực tiếp trên website của hãng hàng không nổi tiếng</b>
+			<br></br>
+			<b> Rõ ràng: Chúng tôi đảm bảo rằng quí khách có thể thao tác dễ dàng, nhanh chóng và hiệu quả khi sử dụng website của chúng tôi</b>
+			<br></br>
+			<b>Email liên hệ: phuong9524hth@gmail.com 
+			
+	</form>	
+	
+	</body>
+</html>
